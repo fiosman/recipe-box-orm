bdb4ca96066ffd9403573b6623af3d8cfbf40637
fiosman <fares.osman@outlook.com>
2022-04-06 23:59:57 -0400

    Adds line

--- data-access-layer/instructions-repository.js
+++ data-access-layer/instructions-repository.js
@@ -1,26 +1,25 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 let Instruction;
 let moduleError;
 
 try {
-  const db = require('../models');
+  const db = require("../models");
   ({ Instruction } = db);
   if (Instruction === undefined) {
-    moduleError = 'It looks like you need to generate the Instruction model.';
+    moduleError = "It looks like you need to generate the Instruction model.";
   }
 } catch (e) {
   console.error(e);
-  if (e.message.includes('Cannot find module')) {
-    moduleError = 'It looks like you need initialize your project.';
+  if (e.message.includes("Cannot find module")) {
+    moduleError = "It looks like you need initialize your project.";
   } else {
     moduleError = `An error was raised "${e.message}". Check the console for details.`;
   }
 }
 /* Don't change code above this line ******************************************/
 
-
-
 async function createNewInstruction(specification, recipeId) {
+  Instruction.findAll({ where: { recipeId: recipeId } });
   // Use the findAll method of the Instruction object to find all the
   // instructions for the specified recipe.
   //
@@ -29,8 +28,6 @@ async function createNewInstruction(specification, recipeId) {
   //
   // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
 }
-
-
 
 /* Don't change code below this line ******************************************/
 module.exports = {
