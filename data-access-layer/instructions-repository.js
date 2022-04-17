6cf114e2bb6588c2435558120614d0090f5a3db9
fiosman <fares.osman@outlook.com>
2022-04-16 22:18:09 -0400

    Update instructions

--- data-access-layer/instructions-repository.js
+++ data-access-layer/instructions-repository.js
@@ -21,8 +21,8 @@ try {
 async function createNewInstruction(specification, recipeId) {
   // Use the findAll method of the Instruction object to find all the
   // instructions for the specified recipe.
-  const allInstructions = Instruction.findAll({ where: { recipeId: recipeId } });
-
+  const allInstructions = await Instruction.findAll({ where: { recipeId: recipeId } });
+  return allInstructions;
   // Use the create method of the Instruction object to create a new object and
   // return it using the maximum listOrder from the query just before this.
   // Docs: https://sequelize.org/v5/manual/instances.html#creating-persistent-instances
